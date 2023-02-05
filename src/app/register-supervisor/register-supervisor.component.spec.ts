00c9599487502fa59e9102d9d1ad6d25a76dcd05
Anas <lakchouchanas@gmail.com>
2023-02-05 15:56:51 +0100

    add student registration form part 2

---
+++ src/app/register-supervisor/register-supervisor.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { RegisterSupervisorComponent } from './register-supervisor.component';
+
+describe('RegisterSupervisorComponent', () => {
+  let component: RegisterSupervisorComponent;
+  let fixture: ComponentFixture<RegisterSupervisorComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ RegisterSupervisorComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(RegisterSupervisorComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});

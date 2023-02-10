bc146ec9a8a853530a7da61cffcb25ba7020b2bd
Anas <lakchouchanas@gmail.com>
2023-02-10 21:24:05 +0100

    fix filters

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -113,19 +113,28 @@ export class HomeComponent implements OnInit {
 
   universityChanged(event: any) {
     this.filter.selectedUniversity = event.target.value;
-    this.establishmentService.getEstablishmentsByUniversity(this.filter.selectedUniversity).subscribe(response => {
-      this.establishments = response;
-    });
-    this.page = 1;
-    this.retrieveSubject();
+    if(!this.filter.selectedUniversity) {
+      this.establishments = [];
+      this.departments = [];
+    } else {
+      this.establishmentService.getEstablishmentsByUniversity(this.filter.selectedUniversity).subscribe(response => {
+        this.establishments = response;
+      });
+      this.page = 1;
+      this.retrieveSubject();
+    }
   }
 
   establishmentChanged(event: any) {
     this.filter.selectedEstablishment = event.target.value;
-    this.departmentService.getDepartmentByEstablishment(this.filter.selectedEstablishment).subscribe(response => {
-      this.departments = response;
-    });
-    this.retrieveSubject();
+    if(!this.filter.selectedEstablishment) {
+      this.departments = [];
+    } else {
+      this.departmentService.getDepartmentByEstablishment(this.filter.selectedEstablishment).subscribe(response => {
+        this.departments = response;
+      });
+      this.retrieveSubject();
+    }
   }
 
   departmentChanged(event: any) {
